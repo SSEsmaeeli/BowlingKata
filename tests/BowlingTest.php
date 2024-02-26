253d82adf703ee308c43ffd65208de3c9e2529de
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2024-02-26 05:19:34 +0330

    add test

---
+++ tests/BowlingTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sajjad\BowlingKata\BowlingGame;
+
+class BowlingTest extends TestCase
+{
+    public function testSuccessfulCase()
+    {
+        $this->assertTrue(true);
+    }
+}
